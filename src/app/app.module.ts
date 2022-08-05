1484bef745370051e402c28fc80472263a05ace6
Abhishek Jamwal <jamwal.ab@gmail.com>
2022-08-05 16:56:33 -0400

    Angular peoject setup for components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IncidentCreateComponent } from './incident/incident-create/incident-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IncidentCreateComponent
   ],
   imports: [
     BrowserModule
