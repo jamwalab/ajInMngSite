84d29028483291541bccaa204393d85043a6f64d
Abhishek Jamwal <jamwal.ab@gmail.com>
2022-08-06 10:22:00 -0400

    home page hero section and services section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { IncidentCreateComponent } from './incident/incident-create/incident-create.component';
+import { HeaderComponent } from './header/header.component';
+import {HomePageComponent} from './homePage/homePage.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IncidentCreateComponent
+    IncidentCreateComponent,
+    HeaderComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
